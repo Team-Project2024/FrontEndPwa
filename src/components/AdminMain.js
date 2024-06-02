002a0ef234908f2e71aea33a5a0f2793642fa2c2
evaain706 <evaain706@gmail.com>
2024-06-02 17:12:44 +0900

    세부페이지 수정

--- src/components/AdminMain.js
+++ src/components/AdminMain.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useContext } from "react";
+import React, { useContext } from "react";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import AuthContext from "../context/AuthProvider";
 import { FaTrashAlt, FaPlus } from "react-icons/fa";
@@ -8,35 +8,18 @@ import DialogContent from '@mui/material/DialogContent';
 import DialogActions from '@mui/material/DialogActions';
 import Button from '@mui/material/Button';
 
-
-
-
-
-
 const AdminMain = () => {
   const { auth } = useContext(AuthContext);
   const axiosPrivate = useAxiosPrivate();
- 
-
-
- 
-
-  
 
   return (
-    <div className="container mx-auto p-8">
-    
-
-    <div className="mb-8 flex justify-center ">
-
-      <h2>안녕</h2>
-
-
+    <div className="flex flex-col items-center justify-center min-h-screen overflow-hidden ">
+      <div className="flex flex-col items-center">
+        <h2 className="font-gmarket text-3xl mb-8">관리자 페이지</h2>
+        <div className="font-gmarket text-2xl">
+          <h2>메뉴에서 추가/관리하고싶은 부분을 선택해주세요!</h2>
+        </div>
       </div>
-
-
-
-   
     </div>
   );
 };
--- src/components/Detail.js
+++ src/components/Detail.js
@@ -3,6 +3,8 @@ import { useNavigate, useParams } from "react-router-dom";
 import useAxiosPrivate from "../hooks/useAxiosPrivate";
 import { Tooltip } from 'react-tooltip';
 import { FaArrowLeft } from 'react-icons/fa';
+import { Doughnut } from 'react-chartjs-2';
+import 'chart.js/auto'; // Chart.js 자동 등록
 
 const DetailPage = () => {
   const { itemType, itemId } = useParams();
@@ -52,117 +54,100 @@ const DetailPage = () => {
     }
   };
 
+  const techData = {
+    labels: ['T영역', 'E영역', 'C영역', 'H영역'],
+    datasets: [
+      {
+        data: [
+          detailInfo.teamwork,
+          detailInfo.entrepreneurship,
+          detailInfo.credit,
+          detailInfo.harnessingResource
+        ],
+        backgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56',
+          '#4BC0C0'
+        ],
+        hoverBackgroundColor: [
+          '#FF6384',
+          '#36A2EB',
+          '#FFCE56',
+          '#4BC0C0'
+        ]
+      }
+    ]
+  };
+
+  const techOptions = {
+    maintainAspectRatio: false,
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      }
+    }
+  };
+
   return (
-    <div className={`min-h-screen flex flex-col items-center p-4 justify-center ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-white text-black'}`}>
-      <div className={`rounded-lg w-full max-w-4xl p-8 items-center justify-center ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'}`}>
+    <div className={`min-h-screen flex flex-col items-center p-4 justify-center ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-slate-300 text-black'}`}>
+      <div className={`rounded-lg w-full max-w-6xl  p-8 items-center justify-center ${isDarkMode ? 'bg-gray-800 text-white' : 'bg-gray-200 text-black'}`}>
         <div className="flex justify-between items-center mb-8">
           <h1 className="text-4xl font-bold">강의세부정보</h1>
           <button 
-            className="p-2 rounded-md" 
+            className={`p-2 rounded-md bg-gray-300 hover:bg-blue-300 transition-colors duration-300 ${isDarkMode ? 'bg-gray-900 text-white' : 'bg-slate-300 text-black'}`}
             data-tooltip-id="my-tooltip" data-tooltip-content="챗봇페이지로 이동"
-            onClick={() => navigate(-1)}  // 뒤로가기 기능 추가
+            onClick={() => navigate(-1)}  // 뒤로가기
           >
             <FaArrowLeft />
           </button>
           <Tooltip id="my-tooltip"/>
         </div>
         <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <div className="space-y-4">
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">강의명</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo === null ? "*" : detailInfo.lectureName}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">이수구분</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.classification}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">강의실</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.room}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">학점</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.credit}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">분반</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.division}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">개설 학년</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.grade}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">T.E.C.H</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.teamwork +
-                  " - " +
-                  detailInfo.entrepreneurship +
-                  " - " +
-                  detailInfo.credit +
-                  " - " +
-                  detailInfo.harnessingResource}
-              </span>
-            </div>
-          
+          <div className="space-y-4 font-gmarket">
+            {[
+              { label: "강의명", value: detailInfo.lectureName },
+              { label: "이수구분", value: detailInfo.classification },
+              { label: "강의실", value: detailInfo.room },
+              { label: "학점", value: detailInfo.credit },
+              { label: "분반", value: detailInfo.division },
+              { label: "개설 학년", value: detailInfo.grade },
+              { label: "교수", value: detailInfo.memberName },
+            ].map((item) => (
+              <div key={item.label} className="flex justify-between items-center">
+                <span className="font-semibold">{item.label}</span>
+                <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
+                  {item.value}
+                </span>
+              </div>
+            ))}
           </div>
-          <div className="space-y-4">
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">AISW디그리</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.aiSw ? "O" : "X"}
-              </span>
+          <div className="space-y-4 font-gmarket">
+            {[
+              { label: "AISW디그리", value: detailInfo.aiSw ? "O" : "X" },
+              { label: "수업방식", value: detailInfo.classMethod },
+              { label: "시험유형", value: detailInfo.testType },
+              { label: "팀플유무", value: detailInfo.teamPlay ? "O" : "X" },
+              { label: "성적산출방식", value: detailInfo.gradeMethod },
+              { label: "강의 시간", value: detailInfo.lectureTime },
+            
+            ].map((item) => (
+              <div key={item.label} className="flex justify-between items-center">
+                <span className="font-semibold">{item.label}</span>
+                <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
+                  {item.value}
+                </span>
+              </div>
+            ))}
+          </div>
+          <div className="col-span-1 md:col-span-2">
+            <h2 className="text-2xl font-bold mb-4">T.E.C.H</h2>
+            <div className={`p-4 rounded-lg ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
+              <div className="relative h-64 w-full">
+                <Doughnut data={techData} options={techOptions} />
+              </div>
             </div>
-          
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">수업방식</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.classMethod}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">시험유형</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.testType}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">팀플유무</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.teamPlay ? "O" : "X"}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">성적산출방식</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.gradeMethod}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">강의 시간</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.lectureTime}
-              </span>
-            </div>
-            <div className="flex justify-between items-center">
-              <span className="font-semibold">교수</span>
-              <span className={`p-2 rounded-md w-2/3 ${isDarkMode ? 'bg-gray-700' : 'bg-gray-300'}`}>
-                {detailInfo.memberName}
-              </span>
-            </div>
-          
           </div>
         </div>
       </div>
